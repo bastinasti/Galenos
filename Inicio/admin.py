fe6e1caa7995dece1e1a6337e09ac6e8755cb380
Bastian Albornoz <bast.albornoz@duocuc.cl>
2022-11-11 02:38:15 -0300

    aweonao

--- Inicio/admin.py
+++ Inicio/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import TipoUsuario, Usuario
+from .models import TipoUsuario, Usuario, Persona, Prevision, Especialidad, Medico, Paciente, MedicoEspec
 
 # Register your models here.
 admin.site.register(TipoUsuario)
 admin.site.register(Usuario)
+admin.site.register(Persona)
+admin.site.register(Prevision)
+admin.site.register(Especialidad)
+admin.site.register(Medico)
+admin.site.register(Paciente)
+admin.site.register(MedicoEspec)
 
