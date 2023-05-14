ebfe48a9d1b4f759ff0b4505db13efb3cb8f83c1
EnoJano <al.escobedo@duocuc.cl>
2023-05-13 22:34:03 -0400

    CAMBIOS
    Creacion de registro
    Cambios en index
    los quiero <3

--- Inicio/urls.py
+++ Inicio/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import inicio, registro, registro2, iniciosesion, registrar_usuario, iniciar_sesion, salir, registrar_paciente
+from .views import inicio, registro, registro2, iniciosesion, registrar_usuario, iniciar_sesion, salir, registrar_paciente, registroUsuario
 
 urlpatterns =[
     path('',inicio, name="inicio"),
     path('registro/', registro, name="registro"),
     path('registro2/', registro2, name="registro2"),
+    path('registroUsuario/', registroUsuario, name="registroUsuario"),
     path('registrar_paciente/', registrar_paciente, name="registrar_paciente"),
     path('registrar_usuario', registrar_usuario, name="registrar_usuario"),
     path('iniciosesion/', iniciosesion, name="iniciosesion"),
