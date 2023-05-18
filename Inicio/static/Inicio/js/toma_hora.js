929e2c99f17438a8bcc041bcde52e481b66970f5
ClaudioOpazo <cla.opazo@duocuc.cl>
2023-05-17 22:10:00 -0400

    cambios

--- Inicio/static/Inicio/js/toma_hora.js
+++ Inicio/static/Inicio/js/toma_hora.js
@@ -1,35 +1,57 @@
-// Obtener referencias a los elementos select
-const select1 = document.getElementById('select1');
-const select2 = document.getElementById('select2');
+// Obtener referencia a los elementos select
+const especialidadSelect = document.getElementById("select1");
+const medicoSelect = document.getElementById("select2");
 
-// Definir los datos para rellenar el segundo select
-const opcionesSelect2 = {
-  opcion1: ["Opción 1.1", "Opción 1.2", "Opción 1.3"],
-  opcion2: ["Opción 2.1", "Opción 2.2", "Opción 2.3"],
-  opcion3: ["Opción 3.1", "Opción 3.2", "Opción 3.3"]
-};
+// Escuchar el evento de cambio en el select de especialidad
+especialidadSelect.addEventListener("change", function () {
+  // Obtener el valor seleccionado de la especialidad
+  const especialidadValue = especialidadSelect.value;
 
-// Función para actualizar el contenido del segundo select
-function actualizarSelect2() {
-  // Obtener el valor seleccionado del primer select
-  const valorSelect1 = select1.value;
+  // Limpiar todas las opciones del select de médico
+  medicoSelect.innerHTML = '<option value="">Seleccionar</option>';
 
-  // Limpiar el contenido actual del segundo select
-  select2.innerHTML = '';
-
-  // Obtener las opciones correspondientes al valor seleccionado en el primer select
-  const opciones = opcionesSelect2[valorSelect1];
-
-  // Crear y añadir las opciones al segundo select
-  opciones.forEach(opcion => {
-    const option = document.createElement('option');
-    option.textContent = opcion;
-    select2.appendChild(option);
-  });
-}
-
-// Escuchar los cambios en el primer select
-select1.addEventListener('change', actualizarSelect2);
-
-// Actualizar el contenido del segundo select al cargar la página
-actualizarSelect2();
+  // Verificar la especialidad seleccionada y agregar las opciones correspondientes
+  if (especialidadValue === "1") {
+    // Agregar la opción de "Manuel Reyes" al select de médico
+    const option = document.createElement("option");
+    const option2 = document.createElement("option");
+    const option3 = document.createElement("option");
+    option.value = "1";
+    option2.value = "2";
+    option3.value = "3";
+    option.textContent = "Claudio Opazo";
+    option2.textContent = "Jorge Zabaleta";
+    option3.textContent = "Pancho Sabedra";
+    medicoSelect.appendChild(option);
+    medicoSelect.appendChild(option2);
+    medicoSelect.appendChild(option3);
+  } else if (especialidadValue === "2") {
+    // Agregar la opción de "Maria Gonzales" al select de médico
+    const option = document.createElement("option");
+    const option3 = document.createElement("option");
+    const option4 = document.createElement("option");
+    option.value = "2";
+    option3.value = "3";
+    option4.value = "4";
+    option.textContent = "Maria Gonzales";
+    option3.textContent = "Alexis Sanchez";
+    option4.textContent = "Ben Brereton";
+    medicoSelect.appendChild(option);
+    medicoSelect.appendChild(option3);
+    medicoSelect.appendChild(option4);
+  } else if (especialidadValue === "3") {
+    // Agregar la opción de "Leonardo Dicaprio" al select de médico
+    const option = document.createElement("option");
+    const option4 = document.createElement("option");
+    const option5 = document.createElement("option");
+    option.value = "3";
+    option4.value = "4";
+    option5.value = "5";
+    option.textContent = "Leonardo Dicaprio";
+    option4.textContent = "Nayaret Albornoz";
+    option5.textContent = "Victor Rosendo";
+    medicoSelect.appendChild(option);
+    medicoSelect.appendChild(option4);
+    medicoSelect.appendChild(option5);
+  }
+});
